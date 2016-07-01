651c20fabaae95c672ed277d40359a3596758ca2
vagrant <vagrant@precise32.(none)>
2016-07-01 04:05:58 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chessapp::Application.config.secret_key_base = '5e05de3496db96c492fcbdc19a4a0dc8c17f46b660d2bb4ae3c2d0ace3b31e83c92aec2c161055ab34d4ac4bed2f5dfbb8a7ce9c056716a75444141b9a3171cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chessapp::Application.config.session_store :cookie_store, key: '_chessapp_session'
