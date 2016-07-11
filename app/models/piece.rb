f9ee5483b68515727164e4c382597f6437cfcb8f
Kevin Hathorn <hathornk@hotmail.com>
2016-07-11 05:51:24 +0000

    Added subclasses to Piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,19 @@
 class Piece < ActiveRecord::Base
+  self.inheritance_column = :type
+  
   belongs_to :user
   belongs_to :game
+
+  def self.types
+    %w(Pawn Rook Bishop Knight Queen King)
+  end
+
 end
+
+
+class Pawn < Piece; end
+class Rook < Piece; end
+class Bishop < Piece; end
+class Knight < Piece; end
+class Queen < Piece; end
+class King < Piece; end
