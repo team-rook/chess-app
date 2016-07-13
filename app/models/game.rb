3df36f56f4af2ac4cc2e6b5ed56f8b8ab5a89677
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-13 22:34:11 +0000

    updated with master update

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,42 @@ class Game < ActiveRecord::Base
   belongs_to :white_user, :class_name => 'User'
   belongs_to :black_user, :class_name => 'User'
   has_many :pieces
-
   scope :needing_second_player, -> { where(black_user: nil) }
+  after_create :initialize_board!
 
+  def initialize_board!
+    (0..7).each do |i|
+      Pawn.create(color: 'black', x_position: i, y_position: 1, game_id: id)
+    end
+
+    Rook.create(color: 'black', x_position: 0, y_position: 0, game_id: id)
+    Rook.create(color: 'black', x_position: 7, y_position: 0, game_id: id)
+
+    Knight.create(color: 'black', x_position: 1, y_position: 0, game_id: id)
+    Knight.create(color: 'black', x_position: 6, y_position: 0, game_id: id)
+
+    Bishop.create(color: 'black', x_position: 2, y_position: 0, game_id: id)
+    Bishop.create(color: 'black', x_position: 5, y_position: 0, game_id: id)
+
+    Queen.create(color: 'black', x_position: 3, y_position: 0, game_id: id)
+    King.create(color: 'black', x_position: 4, y_position: 0, game_id: id)
+
+    (0..7).each do |i|
+      Pawn.create(color: 'white', x_position: i, y_position: 7, game_id: id)
+    end
+
+    Rook.create(color: 'white', x_position: 0, y_position: 7, game_id: id)
+    Rook.create(color: 'white', x_position: 7, y_position: 7, game_id: id)
+
+    Knight.create(color: 'white', x_position: 1, y_position: 7, game_id: id)
+    Knight.create(color: 'white', x_position: 6, y_position: 7, game_id: id)
+
+    Bishop.create(color: 'white', x_position: 2, y_position: 7, game_id: id)
+    Bishop.create(color: 'white', x_position: 5, y_position: 7, game_id: id)
+
+    Queen.create(color: 'white', x_position: 3, y_position: 7, game_id: id)
+    King.create(color: 'white', x_position: 4, y_position: 7, game_id: id)
+  end
 
 
 end
