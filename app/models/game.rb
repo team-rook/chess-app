0718249d1a7c59bf923ccfe757bef7a893e109bd
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-13 02:19:05 +0000

    updated game logic to only include x y coord for pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,37 +6,37 @@ class Game < ActiveRecord::Base
 
   def initialize_board!
     (0..7).each do |i|
-      Pawn.create(color: black, x_position: i, y_position: 1)
+      Pawn.create(x_position: i, y_position: 1)
     end
 
-    Rook.create(color: black, x_position: 0, y_position: 0)
-    Rook.create(color: black, x_position: 7, y_position: 0)
+    Rook.create(x_position: 0, y_position: 0)
+    Rook.create(x_position: 7, y_position: 0)
 
-    Knight.create(color: black, x_position: 1, y_position: 0)
-    Knight.create(color: black, x_position: 6, y_position: 0)
+    Knight.create(x_position: 1, y_position: 0)
+    Knight.create(x_position: 6, y_position: 0)
 
-    Bishop.create(color: black, x_position: 2, y_position: 0)
-    Bishop.create(color: black, x_position: 5, y_position: 0)
+    Bishop.create(x_position: 2, y_position: 0)
+    Bishop.create(x_position: 5, y_position: 0)
 
-    Queen.create(color: black, x_position: 3, y_position: 0)
+    Queen.create(x_position: 3, y_position: 0)
 
-    King.create(color: black, x_position: 4, y_position: 0)
+    King.create(x_position: 4, y_position: 0)
 
     (0..7).each do |i|
-      Pawn.create(color: white, x_position: i, y_position: 7)
+      Pawn.create(x_position: i, y_position: 7)
     end
 
-    Rook.create(color: white, x_position: 0, y_position: 7)
-    Rook.create(color: white, x_position: 7, y_position: 7)
+    Rook.create(x_position: 0, y_position: 7)
+    Rook.create(x_position: 7, y_position: 7)
 
-    Knight.create(color: white, x_position: 1, y_position: 7)
-    Knight.create(color: white, x_position: 6, y_position: 7)
+    Knight.create(x_position: 1, y_position: 7)
+    Knight.create(x_position: 6, y_position: 7)
 
-    Bishop.create(color: white, x_position: 2, y_position: 7)
-    Bishop.create(color: white, x_position: 5, y_position: 7)
+    Bishop.create(x_position: 2, y_position: 7)
+    Bishop.create(x_position: 5, y_position: 7)
 
-    Queen.create(color: white, x_position: 3, y_position: 7)
+    Queen.create(x_position: 3, y_position: 7)
 
-    King.create(color: white, x_position: 4, y_position: 7)
+    King.create(x_position: 4, y_position: 7)
   end
 end
