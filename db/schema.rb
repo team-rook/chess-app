54e61d13fcc5d8aae8fe0c4f66e4bb452d0426bb
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-13 02:13:27 +0000

    ran rake:db rollback in order to avoid conflicting code with updates from kim

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713015405) do
+ActiveRecord::Schema.define(version: 20160709010138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160713015405) do
     t.datetime "updated_at"
     t.integer  "x_position"
     t.integer  "y_position"
-    t.string   "color"
+    t.string   "type"
   end
 
   create_table "users", force: true do |t|
