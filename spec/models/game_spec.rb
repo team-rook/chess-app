52b913ee7ff164fd9e829a048035fb2c0f9f6734
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-15 20:27:50 +0000

    working on tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe 'find_piece should work' do
     it 'should return the piece that is on the specified (x_position, y_position)' do
-      a = Piece.create(x_position: 0, y_position: 0, user_id: 0, game_id: 0)
-      expect(Game.pieces.find_piece(0,0).to eq(a)
+      funsies = Game.create(white_user_id: 0, black_user_id: 1)
+      piece1 = 
     end
   end
 end
