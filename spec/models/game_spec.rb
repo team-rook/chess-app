4659a7f9f45381d82dd7a1fecb9c9e8eaa40de6a
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-15 20:43:57 +0000

    building tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   describe 'find_piece should work' do
-    it 'should return the piece that is on the specified (x_position, y_position)' do
-      funsies = Game.create(white_user_id: 0, black_user_id: 1)
-      piece1 = 
-    end
+
   end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
   describe 'is_obstructed? should work' do
-    it 'should return true if a piece is in the horizonal path' do
-      
-    end
+
   end
 end
