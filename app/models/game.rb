1c1e557458d06a21e2db298b49065da4a5009049
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-15 14:13:36 +0000

    initialize board should be in ine with team mtg discussion

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,42 +2,41 @@ class Game < ActiveRecord::Base
   belongs_to :white_user, :class_name => 'User'
   belongs_to :black_user, :class_name => 'User'
   has_many :pieces
-  after_create :initialize_board!
 
   scope :needing_second_player, -> { where(black_user: nil) }
 
 
   def initialize_board!
     (0..7).each do |i|
-      Pawn.create(color: 'black', x_position: i, y_position: 1, game_id: id)
+      Pawn.create(x_position: i, y_position: 1, game_id: id, user_id: black_user_id)
     end
 
-    Rook.create(color: 'black', x_position: 0, y_position: 0, game_id: id)
-    Rook.create(color: 'black', x_position: 7, y_position: 0, game_id: id)
+    Rook.create(x_position: 0, y_position: 0, game_id: id, user_id: black_user_id)
+    Rook.create(x_position: 7, y_position: 0, game_id: id, user_id: black_user_id)
 
-    Knight.create(color: 'black', x_position: 1, y_position: 0, game_id: id)
-    Knight.create(color: 'black', x_position: 6, y_position: 0, game_id: id)
+    Knight.create(x_position: 1, y_position: 0, game_id: id, user_id: black_user_id)
+    Knight.create(x_position: 6, y_position: 0, game_id: id, user_id: black_user_id)
 
-    Bishop.create(color: 'black', x_position: 2, y_position: 0, game_id: id)
-    Bishop.create(color: 'black', x_position: 5, y_position: 0, game_id: id)
+    Bishop.create(x_position: 2, y_position: 0, game_id: id, user_id: black_user_id)
+    Bishop.create(x_position: 5, y_position: 0, game_id: id, user_id: black_user_id)
 
-    Queen.create(color: 'black', x_position: 3, y_position: 0, game_id: id)
-    King.create(color: 'black', x_position: 4, y_position: 0, game_id: id)
+    Queen.create(x_position: 3, y_position: 0, game_id: id, user_id: black_user_id)
+    King.create(x_position: 4, y_position: 0, game_id: id, user_id: black_user_id)
 
     (0..7).each do |i|
-      Pawn.create(color: 'white', x_position: i, y_position: 7, game_id: id)
+      Pawn.create(x_position: i, y_position: 7, game_id: id, user_id: white_user_id)
     end
 
-    Rook.create(color: 'white', x_position: 0, y_position: 7, game_id: id)
-    Rook.create(color: 'white', x_position: 7, y_position: 7, game_id: id)
+    Rook.create(x_position: 0, y_position: 7, game_id: id, user_id: white_user_id)
+    Rook.create(x_position: 7, y_position: 7, game_id: id, user_id: white_user_id)
 
-    Knight.create(color: 'white', x_position: 1, y_position: 7, game_id: id)
-    Knight.create(color: 'white', x_position: 6, y_position: 7, game_id: id)
+    Knight.create(x_position: 1, y_position: 7, game_id: id, user_id: white_user_id)
+    Knight.create(x_position: 6, y_position: 7, game_id: id, user_id: white_user_id)
 
-    Bishop.create(color: 'white', x_position: 2, y_position: 7, game_id: id)
-    Bishop.create(color: 'white', x_position: 5, y_position: 7, game_id: id)
+    Bishop.create(x_position: 2, y_position: 7, game_id: id, user_id: white_user_id)
+    Bishop.create(x_position: 5, y_position: 7, game_id: id, user_id: white_user_id)
 
-    Queen.create(color: 'white', x_position: 3, y_position: 7, game_id: id)
-    King.create(color: 'white', x_position: 4, y_position: 7, game_id: id)
+    Queen.create(x_position: 3, y_position: 7, game_id: id, user_id: white_user_id)
+    King.create(x_position: 4, y_position: 7, game_id: id, user_id: white_user_id)
   end
 end
