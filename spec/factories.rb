ccc14cde522dabb8bd30a76cce3527be0e6923f9
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-16 17:08:20 +0000

    building factorygirl for each piece type

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,30 @@ FactoryGirl.define do
     password "secretPassword"
     password_confirmation "secretPassword"
   end
+
+  factory :piece do
+    factory :pawn do
+      type "Pawn"
+    end
+
+    factory :rook do
+      type "Rook"
+    end
+
+    factory :knight do
+      type "Knight"
+    end
+
+    factory :bishop do
+      type "Bishop"
+    end
+
+    factory :king do
+      type "King"
+    end
+
+    factory :queen do
+      type "Queen"
+    end
+  end
 end
