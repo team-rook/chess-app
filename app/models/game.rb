478819881b25330d67cc79283d824a84db901cb7
vagrant <ahbabousara@gmail.com>
2016-07-17 17:57:54 +0000

    Added ability to join 2nd game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
-  belongs_to :white_user, :class_name => 'User', foreign_key: "white_user_id"
-  belongs_to :black_user, :class_name => 'User', foreign_key: "black_user_id"
+  belongs_to :white_user, :class_name => 'User'
+  belongs_to :black_user, :class_name => 'User'
   has_many :pieces
 
   scope :needing_second_player, -> { where(black_user_id: nil) }
