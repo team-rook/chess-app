4d978fefbe438f5e7dd7ccba60c059a1d5130edc
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-07-18 23:25:29 +0000

    added captured field to piece table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713195852) do
+ActiveRecord::Schema.define(version: 20160717231536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160713195852) do
     t.datetime "updated_at"
     t.integer  "x_position"
     t.integer  "y_position"
+    t.boolean  "captured"
   end
 
   create_table "users", force: true do |t|
