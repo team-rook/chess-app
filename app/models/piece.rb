2c45f23948d0aaf31923c30a77358e6b008f110e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-18 23:41:58 +0000

    making progress

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -24,4 +24,53 @@ class Piece < ActiveRecord::Base
   def y_diff(y)
     (y - y_position).abs
   end
+
+  # returns true if attempting horizontal move
+  def horizontal?(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    return (x_diff > 0) && (y_diff == 0)
+  end
+
+  # returns true if attempting vertical move
+  def vertical?(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    return (x_diff == 0) && (y_diff > 0)
+  end
+
+  # returns true if attempting diagonal move
+  def diagonal?(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    return (x_diff) == (y_diff)
+  end
+
+  # compares current x and destination x and returns the smaller value
+  def x_min(x)
+    [x_position, x].min
+  end
+
+  # compares current x and destination x and returns the larger value
+  def x_max(x)
+    [x_position, x].max
+  end
+
+  # compares current y and destination y and returns the smaller value
+  def y_min(y)
+    [y_position, y].min
+  end
+
+  # compares current y and destination y and returns the larger value
+  def y_max(y)
+    [y_position, y].max
+  end
+
+  # returns array of coordinates along the path of move
+  def path_clear?
+    return vertical_clear? if vertical?
+    return horizontal_clear? if horizontal?
+    return diagonal_clear? if diagonal?
+    fail ArgumentError, 'Invalid path'
+  end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
+  describe 'horizontal?' do
+    it 'should return true if piece is moving horizontally east' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.horizontal?(7,0)).to eq true
+    end
 
+    it 'should return true if piece is moving horizontally west' do
+      rook = Rook.create(x_position: 7, y_position: 0)
+      expect(rook.horizontal?(2,0)).to eq true
+    end
+
+    it 'should return false if not horizontal move' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.horizontal?(0,4)).to eq false
+    end
+  end
+
+  describe 'vertical?' do
+    it 'should return true if the piece is moving vertically north' do
+      rook = Rook.create(x_position: 0, y_position: 7)
+      expect(rook.vertical?(0,5)).to eq true
+    end
+
+    it 'should return true if the piece is moving vertically south' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.vertical?(0,5)).to eq true
+    end
+
+    it 'should return false if not vertical move' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.vertical?(7,0)).to eq false
+    end
+  end
+
+  describe 'diagonal?' do
+    it 'should return true if the piece is moving diagonally southeast' do
+      bishop = Bishop.create(x_position: 2, y_position: 0)
+      expect(bishop.diagonal?(4,2)).to eq true
+    end
+
+    it 'should return true if the piece is moving diagonally southwest' do
+      bishop = Bishop.create(x_position: 2, y_position: 0)
+      expect(bishop.diagonal?(0,2)).to eq true
+    end
+
+    it 'should return true if the piece is moving diagonally northwest' do
+      bishop = Bishop.create(x_position: 5, y_position: 7)
+      expect(bishop.diagonal?(7,5)).to eq true
+    end
+
+    it 'should return true if the piece is moving diagonally northeast' do
+      bishop = Bishop.create(x_position: 5, y_position: 7)
+      expect(bishop.diagonal?(2,4)).to eq true
+    end
+
+    it 'should return false if not diagonal move' do
+      bishop = Bishop.create(x_position: 5, y_position: 7)
+      expect(bishop.diagonal?(5,4)).to eq false
+    end
+  end
+
+  describe 'x_min' do
+    it 'should compare current x and destination x and return the smaller value' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.x_min(7)).to eq 0
+    end
+  end
+
+  describe 'x_max' do
+    it 'should compare current x and destination x and return the larger value' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.x_max(7)).to eq 7
+    end
+  end
+
+  describe 'y_min' do
+    it 'should compare current y and destination y and return the smaller value' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.y_min(7)).to eq 0
+    end
+  end
+
+  describe 'y_max' do
+    it 'should compare current y and destination y and return the larger value' do
+      rook = Rook.create(x_position: 0, y_position: 0)
+      expect(rook.y_max(7)).to eq 7
+    end
+  end
 end
