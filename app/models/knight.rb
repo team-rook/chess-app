cfaf6a668400d7688c0909ff27ff115c1a1f605f
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-25 16:31:05 +0000

    completed knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,9 +1,22 @@
 class Knight < Piece
-  
-   def unicode_symbol
-      if white?
+  # knight can move 2 squares horizontal and 1 square north or south
+  # knight can move 2 squares vertical and 1 square east or west
+  def valid_move?(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    if ((x_diff == 2) && (y_diff == 1))
+      return true
+    end
+    if ((y_diff == 2) && (x_diff == 1))
+      return true
+    end
+    return false
+  end
+
+  def unicode_symbol
+    if white?
       return "&#9816;"
-      else 
+    else
       return  "&#9822;"
     end
   end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Knight, type: :model do
+  describe 'valid_move?' do
+    it 'should return true if moving two squares north and one square east' do
+      knight = Knight.create(x_position: 2, y_position: 5)
+      expect(knight.valid_move?(3,3)).to eq true
+    end
 
+    it 'should return true if moving two squares north and one square west' do
+      knight = Knight.create(x_position: 2, y_position: 5)
+      expect(knight.valid_move?(1,3)).to eq true
+    end
+
+    it 'should return true if moving two squares south and one square east' do
+      knight = Knight.create(x_position: 1, y_position: 3)
+      expect(knight.valid_move?(2,5)).to eq true
+    end
+
+    it 'should return true if moving two squares west and one square south' do
+      knight = Knight.create(x_position: 3, y_position: 3)
+      expect(knight.valid_move?(1,4)).to eq true
+    end
+
+    it 'should return true if moving two squares east and one square north' do
+      knight = Knight.create(x_position: 3, y_position: 3)
+      expect(knight.valid_move?(5,2)).to eq true
+    end
+
+    it 'should return false if moving to a random square' do
+      knight = Knight.create(x_position: 3, y_position: 3)
+      expect(knight.valid_move?(6,5)).to eq false
+    end
+  end
 end
