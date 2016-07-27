8be39cefe5c8ab59954c3f1150635638611d87f6
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-27 19:10:47 +0000

    need help making sure pawns cannot move backwards

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,26 +1,46 @@
 class Pawn < Piece
-  # returns true if white pawn making its first move
+  # returns true if pawn is moving north
+  def north?(x,y)
+    start_y = self.y_position
+    end_y = y
+    return true if ((start_y - end_y) > 0)
+    return false
+  end
+
+  # returns true if pawn is moving south
+  def south?(x,y)
+    start_y = self.y_position
+    end_y = y
+    return true if ((start_y - end_y) < 0)
+    return false
+  end
+
+  # returns true if white pawn making a legal first move
   def first_move_north?(x,y)
-    if self.y_position == 6
-      if y == 5 || y == 4
-        return true
-      else
-        return false
-      end
-    end
+    return true if (self.y_position == 6) && (y == 5 || y == 4)
+    return false
   end
 
   # returns true if white pawn moving 1 square north
   def one_square_north?(x,y)
     x_diff = x_diff(x)
     y_diff = y_diff(y)
-    start_y = self.y_position
-    end_y = y
-    if (start_y - end_y > 0) && (y_diff == 1) && (x_diff == 0)
-      return true
-    else
-      return false
-    end
+    return true if self.north?(x,y) && y_diff == 1 && x_diff == 0
+    return false
+  end
+
+  # returns true if black pawn making legal first move
+  def first_move_south?(x,y)
+    return true if (self.y_position == 1) && (y == 2 || y == 3)
+    return false
+  end
+
+  # returns true if black pawn moving 1 square south
+  def one_square_south?(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    return true if self.south?(x,y) && y_diff == 1 && x_diff == 0
+    return false
   end
 
   # each pawn can move forward vertically 1 or 2 spaces for its first move
@@ -30,22 +50,21 @@ class Pawn < Piece
     return false unless super(x,y)
     # returns false if path blocked to destination square
     return false if path_blocked?(x,y)
-    x_diff = x_diff(x)
-    y_diff = y_diff(y)
-    # checks for white pawn
+    # check move for white pawn
     if self.white?
       # allow white pawn to move 2 squares north on its first move only
-      if self.first_move_north?(x,y)
-        return true
-      # otherwise can only move 1 square north
-      else
-        if self.one_square_north?(x,y)
-          return true
-        else
-          return false
-        end
-      end
+      return true if self.first_move_north?(x,y)
+      # allow white pawn to move 1 square north on any turn
+      return true if self.one_square_north?(x,y)
     end
+    # check move for black pawn
+    if self.black?
+      # allow black pawn to move 2 squares south on its first move only
+      return true if self.first_move_south?(x,y)
+      # allow white pawn to move 1 square south on any turn
+      return true if self.one_square_south?(x,y)
+    end
+    return false
   end
 
   def unicode_symbol
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -64,16 +64,28 @@ RSpec.describe Pawn, type: :model do
       expect(pawn.valid_move?(7,3)).to be true
     end
 
-    # it 'should return false if white pawn moving 1 square south (backwards)' do
-    #   game = Game.create(white_user_id: 0, black_user_id: 1)
-    #   pawn = Pawn.create(x_position: 7, y_position: 4, game_id: game.id, user_id: 0)
-    #   expect(pawn.valid_move?(7,3)).to be false
-    # end
-    #
-    # it 'should return false if black pawn moving 1 square north (backwards)' do
-    #   game = Game.create(white_user_id: 0, black_user_id: 1)
-    #   pawn = Pawn.create(x_position: 7, y_position: 3, game_id: game.id, user_id: 1)
-    #   expect(pawn.valid_move?(7,2)).to be false
-    # end
+    it 'should return false if black pawn moving 1 square south for any move' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      pawn = Pawn.create(x_position: 7, y_position: 2, game_id: game.id, user_id: 1)
+      expect(pawn.valid_move?(7,3)).to be true
+    end
+
+    it 'should return false if black pawn is making a random move' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      pawn = Pawn.create(x_position: 7, y_position: 2, game_id: game.id, user_id: 1)
+      expect(pawn.valid_move?(4,3)).to be false
+    end
+
+    it 'should return false if white pawn is moving backwards' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: 0)
+      expect(pawn.valid_move?(0,5)).to be false
+    end
+
+    it 'should return false if black pawn is moving backwards' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      pawn = Pawn.create(x_position: 2, y_position: 4, game_id: game.id, user_id: 1)
+      expect(pawn.valid_move?(2,3)).to be false
+    end
   end
 end
