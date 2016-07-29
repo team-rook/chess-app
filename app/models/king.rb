14aa1ecbf30371c989d862e4b895d15c52ec72e5
Kevin Hathorn <hathornk@hotmail.com>
2016-07-29 21:06:21 +0000

    Adding ability to castle, not yet passing all tests

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,8 +1,12 @@
 class King < Piece
-  # ensures king piece can only move 1 square in any direction
+
   def valid_move?(x,y)
-    # checks critera for valid_move? that apply to all pieces and returns false if fails
-    return false unless super(x,y)
+    # checks critera for valid_move? that apply to all pieces and returns false if fails unless can_castle
+    (normal_valid_move?(x,y) && super(x,y)) || can_castle?(x,y)
+  end
+
+  # ensures king piece can only move 1 square in any direction
+  def normal_valid_move?(x,y)
     x_diff = x_diff(x)
     y_diff = y_diff(y)
     # returns true if moving only 1 square any direction
@@ -12,6 +16,35 @@ class King < Piece
     # returns false if not moving only 1 square any direction
     return false
   end
+
+
+  def can_castle?(x,y)
+    updated_at == created_at && \
+    ((x == 6 && castle_rook && castle_rook.x_position == 7 && \
+    castle_rook.updated_at == castle_rook.created_at && \
+    castle_rook.path_blocked?(5, y_position) == false) || \
+    (x == 2 && castle_rook && castle_rook.x_position == 0 && \
+    castle_rook.updated_at == castle_rook.created_at && \
+    castle_rook.path_blocked?(3, y_position) == false )) && \
+    [y, castle_rook.y_position].uniq == y_position
+  end
+
+
+  def castle_rook
+    @castle_rook = game.pieces.where(x_position: 0, type: Rook).first || game.pieces.where(x_position: 7, type: Rook).first
+  end
+
+
+  def castle!
+    castle_rook.move_to((rook.x_position - (rook.x_position - king.x_position).abs + 1), y_position)
+  end
+
+
+  def move_to(x,y)
+    castle! if can_castle?(x,y)
+    super
+  end
+
 
   def unicode_symbol
     if white?
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -2,82 +2,175 @@ require 'rails_helper'
 
 RSpec.describe King, type: :model do
   describe 'valid_move?' do
+     it 'should return true if attempting to castle kingside and can castle' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 7, y_position: 0, game_id: game.id)
+      expect(king.valid_move?(6,0)).to eq true
+    end
+
+     it 'should return true if attempting to castle queenside and can castle' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 0, y_position: 0, game_id: game.id)
+      expect(king.valid_move?(2,0)).to eq true
+    end    
+
+    it 'should return false if attempting to castle kingside and bishop present' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 7, y_position: 0, game_id: game.id)
+      bishop = Bishop.create(x_position: 5, y_position:0, game_id: game.id)
+      expect(king.valid_move?(6,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle kingside and knight present' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 7, y_position: 0, game_id: game.id)
+      knight = Knight.create(x_position: 6, y_position:0, game_id: game.id)
+      expect(king.valid_move?(6,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle queenside and bishop present' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 0, y_position: 0, game_id: game.id)
+      bishop = Bishop.create(x_position: 2, y_position:0, game_id: game.id)
+      expect(king.valid_move?(2,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle queenside and knight present' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 0, y_position: 0, game_id: game.id)
+      knight = Knight.create(x_position: 1, y_position:0, game_id: game.id)
+      expect(king.valid_move?(2,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle queenside and queen present' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 0, y_position: 0, game_id: game.id)
+      queen = Queen.create(x_position: 3, y_position:0, game_id: game.id)
+      expect(king.valid_move?(2,0)).to eq false
+    end    
+
+    it 'should return false if  attempting to castle kingside and 7 rook has been moved' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 7, y_position: 0, game_id: game.id)
+      rook.move_to!(6,0)
+      rook.move_to!(7,0)
+      expect(king.valid_move?(6,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle queenside and 0 rook has been moved' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 0, y_position: 0, game_id: game.id)
+      rook.move_to!(1,0)
+      rook.move_to!(0,0)
+      expect(king.valid_move?(6,0)).to eq false
+    end
+
+    it 'should return false if attempting to castle and king has been moved' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      rook = Rook.create(x_position: 7, y_position: 0, game_id: game.id)
+      king.move_to!(3,0)
+      king.move_to!(4,0)
+      expect(king.valid_move?(6,0)).to eq false
+    end
+
+    it 'should return false if attempting to move 2 squares south' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 0, game_id: game.id)
+      expect(king.valid_move?(4,2)).to eq false
+    end
+
+    it 'should return false if attempting to move 2 squares north' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = King.create(x_position: 4, y_position: 7, game_id: game.id)
+      expect(king.valid_move?(4,5)).to eq false
+    end
+
     it 'should return true if moving one square east' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(5,0)).to eq true
+      expect(king.normal_valid_move?(5,0)).to eq true
     end
 
     it 'should return true if moving one square west' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(3,0)).to eq true
+      expect(king.normal_valid_move?(3,0)).to eq true
     end
 
     it 'should return true if moving one square north' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(4,0)).to eq true
+      expect(king.normal_valid_move?(4,0)).to eq true
     end
 
     it 'should return true if moving one square south' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(4,2)).to eq true
+      expect(king.normal_valid_move?(4,2)).to eq true
     end
 
     it 'should return true if moving one square northeast' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(5,0)).to eq true
+      expect(king.normal_valid_move?(5,0)).to eq true
     end
 
     it 'should return true if moving one square northwest' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(3,0)).to eq true
+      expect(king.normal_valid_move?(3,0)).to eq true
     end
 
     it 'should return true if moving one square southwest' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(3,2)).to eq true
+      expect(king.normal_valid_move?(3,2)).to eq true
     end
 
     it 'should return true if moving one square southeast' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 1, game_id: game.id)
-      expect(king.valid_move?(5,2)).to eq true
+      expect(king.normal_valid_move?(5,2)).to eq true
     end
 
     it 'should return false if moving more than one square east' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(7,0)).to eq false
+      expect(king.normal_valid_move?(7,0)).to eq false
     end
 
     it 'should return false if moving more than one square south' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(4,5)).to eq false
+      expect(king.normal_valid_move?(4,5)).to eq false
     end
 
     it 'should return false if moving more than one square southwest' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(0,4)).to eq false
+      expect(king.normal_valid_move?(0,4)).to eq false
     end
 
     it 'should return false if moving to a random square on the board' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 4, y_position: 0, game_id: game.id)
-      expect(king.valid_move?(2,7)).to eq false
+      expect(king.normal_valid_move?(2,7)).to eq false
     end
 
     it 'should return false if trying to move off the board' do
       game = Game.create(white_user_id: 0, black_user_id: 1)
       king = King.create(x_position: 0, y_position: 2, game_id: game.id)
-      expect(king.valid_move?(-1,8)).to eq false
+      expect(king.normal_valid_move?(-1,8)).to eq false
     end
   end
 end
