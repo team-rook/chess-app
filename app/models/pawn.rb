dda1f06718934df911132ffb2cff5281f41898e4
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-08-05 16:25:51 +0000

    working but open to refactor suggestions

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -9,6 +9,11 @@ class Pawn < Piece
     return ((y_position - y) < 0)
   end
 
+  # returns true if pawn has never moved from home row
+  def on_home_row?
+    return (white? && y_position == 6) || y_position == 1
+  end
+
   # returns true for first move only
   def two_squares?(x,y)
     return on_home_row? && (x_diff(x) == 0) && (y_diff(y) == 2)
@@ -42,6 +47,18 @@ class Pawn < Piece
     # pawns capture one square diagonally
     return true if standard_capture?(x,y)
     return false
+  end
+
+  # moves piece to the destination square
+  def pawn_move_to!(x,y)
+    if valid_move?(x,y) && standard_capture?(x,y)
+      if self.game.square_occupied?(x,y)
+        self.game.find_piece(x,y).captured!
+      end
+      update_attributes(x_position: x, y_position: y)
+      self.move_number += 1
+      self.game.move_counter += 1
+    end
   end
 
   # returns true if unfriendly pawn moved 2 squares on its first move and now can be captured en passant
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,34 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  describe 'unfriendly_pawn?' do
-    it 'should return true if unfriendly pawn at square' do
-      white_user = FactoryGirl.create(:user)
-      black_user = FactoryGirl.create(:user)
-      game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      white_pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
-      black_pawn = Pawn.create(x_position: 1, y_position: 4, game_id: game.id, user_id: black_user.id)
-      expect(white_pawn.unfriendly_pawn?(1,4)).to be true
-    end
-  end
-
-  describe 'en_passant?' do
-    it 'should return true if unfriendly pawn can be captured' do
-      white_user = FactoryGirl.create(:user)
-      black_user = FactoryGirl.create(:user)
-      game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      white_pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
-      black_pawn = Pawn.create(x_position: 1, y_position: 4, game_id: game.id, user_id: black_user.id)
-      expect(black_pawn.en_passant?(0,5)).to be true
-    end
-  end
-
   describe 'valid_move?' do
     it 'should return true if a white pawn is moving 2 squares north for its first move' do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 0, y_position: 6, game_id: game.id, user_id: white_user.id)
+      pawn = Pawn.create(move_number: 0, x_position: 0, y_position: 6, game_id: game.id, user_id: white_user.id)
       expect(pawn.valid_move?(0,4)).to be true
     end
 
@@ -36,7 +14,7 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
+      pawn = Pawn.create(move_number: 1, x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
       expect(pawn.valid_move?(0,2)).to be false
     end
 
@@ -44,7 +22,7 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 7, y_position: 5, game_id: game.id, user_id: white_user.id)
+      pawn = Pawn.create(move_number: 3, x_position: 7, y_position: 5, game_id: game.id, user_id: white_user.id)
       expect(pawn.valid_move?(7,4)).to be true
     end
 
@@ -52,7 +30,7 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 0, y_position: 1, game_id: game.id, user_id: black_user.id)
+      pawn = Pawn.create(move_number: 0, x_position: 0, y_position: 1, game_id: game.id, user_id: black_user.id)
       expect(pawn.valid_move?(0,3)).to be true
     end
 
@@ -60,7 +38,7 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 0, y_position: 3, game_id: game.id, user_id: black_user.id)
+      pawn = Pawn.create(move_number: 1, x_position: 0, y_position: 3, game_id: game.id, user_id: black_user.id)
       expect(pawn.valid_move?(0,5)).to be false
     end
 
@@ -68,7 +46,7 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 7, y_position: 2, game_id: game.id, user_id: black_user.id)
+      pawn = Pawn.create(move_number: 3, x_position: 7, y_position: 2, game_id: game.id, user_id: black_user.id)
       expect(pawn.valid_move?(7,3)).to be true
     end
 
@@ -76,19 +54,19 @@ RSpec.describe Pawn, type: :model do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      pawn = Pawn.create(x_position: 7, y_position: 2, game_id: game.id, user_id: black_user.id)
+      pawn = Pawn.create(move_number: 2, x_position: 7, y_position: 2, game_id: game.id, user_id: black_user.id)
       expect(pawn.valid_move?(4,3)).to be false
     end
   end
 
-  describe 'move_to!' do
+  describe 'pawn_move_to!' do
     it 'should return true if white pawn making standard capture move' do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
       white_pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
       black_pawn = Pawn.create(x_position: 1, y_position: 3, game_id: game.id, user_id: black_user.id)
-      white_pawn.move_to!(1,3)
+      white_pawn.pawn_move_to!(1,3)
       black_pawn.reload
       expect(white_pawn.x_position).to eq 1
       expect(white_pawn.y_position).to eq 3
@@ -101,7 +79,7 @@ RSpec.describe Pawn, type: :model do
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
       white_pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
       black_pawn = Pawn.create(x_position: 1, y_position: 3, game_id: game.id, user_id: black_user.id)
-      black_pawn.move_to!(0,4)
+      black_pawn.pawn_move_to!(0,4)
       white_pawn.reload
       expect(black_pawn.x_position).to eq 0
       expect(black_pawn.y_position).to eq 4
@@ -113,7 +91,7 @@ RSpec.describe Pawn, type: :model do
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
       white_pawn = Pawn.create(x_position: 1, y_position: 3, game_id: game.id, user_id: white_user.id)
-      black_pawn = Pawn.create(x_position: 0, y_position: 3, game_id: game.id, user_id: black_user.id)
+      black_pawn = Pawn.create(move_number: 1, pawn_two_squares: true, x_position: 0, y_position: 3, game_id: game.id, user_id: black_user.id)
       white_pawn.en_passant!(0,2)
       black_pawn.reload
       expect(white_pawn.x_position).to eq 0
@@ -121,41 +99,15 @@ RSpec.describe Pawn, type: :model do
       expect(black_pawn.captured).to eq true
     end
 
-    it 'should return true if white pawn capturing en passant' do
-      white_user = FactoryGirl.create(:user)
-      black_user = FactoryGirl.create(:user)
-      game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      white_pawn = Pawn.create(x_position: 1, y_position: 3, game_id: game.id, user_id: white_user.id)
-      black_pawn = Pawn.create(x_position: 2, y_position: 3, game_id: game.id, user_id: black_user.id)
-      white_pawn.en_passant!(2,2)
-      black_pawn.reload
-      expect(white_pawn.x_position).to eq 2
-      expect(white_pawn.y_position).to eq 2
-      expect(black_pawn.captured).to eq true
-    end
-
     it 'should return true if black pawn capturing en passant' do
       white_user = FactoryGirl.create(:user)
       black_user = FactoryGirl.create(:user)
       game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      white_pawn = Pawn.create(x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
+      white_pawn = Pawn.create(move_number: 1, pawn_two_squares: true, x_position: 0, y_position: 4, game_id: game.id, user_id: white_user.id)
       black_pawn = Pawn.create(x_position: 1, y_position: 4, game_id: game.id, user_id: black_user.id)
       black_pawn.en_passant!(0,5)
       white_pawn.reload
       expect(black_pawn.x_position).to eq 0
-      expect(black_pawn.y_position).to eq 5
-      expect(white_pawn.captured).to eq true
-    end
-
-    it 'should return true if black pawn capturing en passant' do
-      white_user = FactoryGirl.create(:user)
-      black_user = FactoryGirl.create(:user)
-      game = Game.create(white_user_id: white_user.id, black_user_id: black_user.id)
-      white_pawn = Pawn.create(x_position: 2, y_position: 4, game_id: game.id, user_id: white_user.id)
-      black_pawn = Pawn.create(x_position: 1, y_position: 4, game_id: game.id, user_id: black_user.id)
-      black_pawn.en_passant!(2,5)
-      white_pawn.reload
-      expect(black_pawn.x_position).to eq 2
       expect(black_pawn.y_position).to eq 5
       expect(white_pawn.captured).to eq true
     end
