cb4ff12ac30db83c02e343249df8fdd4a3f2e675
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-08-12 00:33:30 +0000

    adding logic to piece controller but still not working

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,12 +1,15 @@
 class PiecesController < ApplicationController
 	def update
 	  @piece = Piece.find(params[:id])
+		x = params[:x_position].to_i
+		y = params[:y_position].to_i
 	  @game = @piece.game
-		if @piece.user_id == current_user.id
-			x = params[:x_position].to_i
-			y = params[:y_position].to_i
-	  	@piece.move_to!(x,y)
-			render json: 'success'
+
+		if @piece.user_id == @game.active_player.id
+			if @piece.valid_move?(x,y)
+				@piece.move_to!(x,y)
+				render json: 'success'
+			end
 		else
 			render json: 'failure'
 		end
@@ -15,6 +18,6 @@ class PiecesController < ApplicationController
 	private
 
 	def piece_params
-		params.require(:piece).permit(:game_id, :user_id, :type, :x_position, :y_position)
+		params.require(:piece).permit(:game_id, :user_id, :type, :x_position, :y_position, :captured)
 	end
 end
