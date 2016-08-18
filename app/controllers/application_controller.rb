3fc4c151d6f78e8c36869ee67e2acd4b560b14fa
Kevin Hathorn <hathornk@hotmail.com>
2016-08-18 18:34:40 +0000

    added pusher snippets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
+    def hello_world
+    Pusher.trigger('test_channel', 'my_event', {
+      message: 'hello world'
+    })
+  end
 end
 
 
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,7 @@
+require 'pusher'
+
+Pusher.app_id = '235077'
+Pusher.key = '20db8464ebd0391533d7'
+Pusher.secret = '760a5e38b606ed70046e'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
