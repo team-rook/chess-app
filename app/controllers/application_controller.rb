2af0a88dd32eb8c05adc41512374b6055365534a
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-08-18 22:28:10 +0000

    working?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    def hello_world
-    Pusher.trigger('test_channel', 'my_event', {
-      message: 'hello world'
-    })
-  end
 end
-
-
